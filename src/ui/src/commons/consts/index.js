f10724cdf3e49321a2cdf852e991a5ea649b3ff5
YuTianhao <th2009yu@mail.com>
2020-04-15 22:56:36 +0800

    modify ui comment

--- src/ui/src/commons/consts/index.js
+++ src/ui/src/commons/consts/index.js
@@ -1,18 +1,4 @@
-// Copyright 2018 Naftis Authors
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//     http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-
-// Task defines some consts about creating task and notifacating executed status of task.
+// Task 定义了一些有关创建Task和通知Task执行状态的常量。
 const Task = {
   command: {
     APPLY: 'apply',
